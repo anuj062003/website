305f80d9648ca24b45e273028ffb4941ecdfd45f
anuj062003 <yanujkumar59@gmail.com>
2025-07-22 13:43:06 +0530

    News Letter Change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsLetter from '../components/NewsLetter'
+import NewsLetter from '../components/Newsletter'
 
 
 const Home = () => {
