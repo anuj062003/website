b5cfe4179fc80beaa0400d6ac36f90285cbd0cb4
anuj062003 <yanujkumar59@gmail.com>
2025-07-22 15:14:06 +0530

    Backend change

--- Backend/server.js
+++ Backend/server.js
@@ -7,13 +7,21 @@ import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
+import cors from 'cors'
 
 //App config
 
 const app=express();
+
+app.use(cors({
+  origin: 'https://forever-website-frontend2.vercel.app',
+  credentials: true // if you're using cookies or sessions
+}));
 const port=process.env.PORT || 4000
 connectDB();
 connectCloudinary();
+
+
 
 
 //Middlewares
